2ffe9a89417372492520d78306df5b08223fd139
Adam Goth <adamgoth@gmail.com>
2017-01-15 17:40:29 -0700

    Adds challenge 19

---
+++ challenge19.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+/*
+ Challenge 19: Swap two numbers
+ Difficulty: Easy
+ Swap two positive variable integers, a and b, without using a temporary variable.
+ Sample input and output
+ • Before running your code a should be 1 and b should be 2; afterwards, b should be 1 and a should be 2.
+*/
+
+var a = 1
+var b = 2
+
+(a, b) = (b, a)
+
+//a = b + a
+//b = a - b
+//a = a - b
+
+a
+b
