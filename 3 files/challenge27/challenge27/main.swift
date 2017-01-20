a7cfbf33dde2f01d24a958a2205cf22a6ae68b32
Adam Goth <adamgoth@gmail.com>
2017-01-20 07:12:20 -0700

    Recreates challenge 27 as a command line project

--- 3 files/challenge27/challenge27/main.swift
+++ 3 files/challenge27/challenge27/main.swift
@@ -1,3 +1,11 @@
+//
+//  main.swift
+//  challenge27
+//
+//  Created by Adam Goth on 1/20/17.
+//  Copyright © 2017 Adam Goth. All rights reserved.
+//
+
 //: Playground - noun: a place where people can play
 
 /*
@@ -18,15 +26,12 @@
  • If asked to print the last 3 lines, your code should output “Twelfth Night, Othello, Macbeth”.
  • If asked to print the last 100 lines, your code should output “Twelfth Night, Othello, Macbeth, King Lear, Julius Caesar, Hamlet, Cymbeline, Coriolanus, Antony and Cleopatra”.
  • If asked to print the last 0 lines, your could should print nothing.
-*/
+ */
 
-import UIKit
-
-
+import Foundation
 
 func printLastLines(filename: String, numberOfLines: Int) {
-    let path = Bundle.main.path(forResource: "input", ofType: "txt")
-    guard let contents = try? String(contentsOfFile: path!) else { return }
+    guard let contents = try? String(contentsOfFile: filename) else { return }
     var lines = contents.components(separatedBy: .newlines).filter { $0 != "" }
     guard lines.count > 0 else { return }
     lines.reverse()
@@ -35,6 +40,8 @@ func printLastLines(filename: String, numberOfLines: Int) {
     }
 }
 
-printLastLines(filename: "input", numberOfLines: 3)
-printLastLines(filename: "input", numberOfLines: 100)
-printLastLines(filename: "input", numberOfLines: 0)
+printLastLines(filename: "/Users/apg/Desktop/input.txt", numberOfLines: 3)
+printLastLines(filename: "/Users/apg/Desktop/input.txt", numberOfLines: 100)
+printLastLines(filename: "/Users/apg/Desktop/input.txt", numberOfLines: 0)
+
+
